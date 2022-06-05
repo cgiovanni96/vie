2177902a4527046b9250792ad5c6e8a220f3d3ad
Giovanni Costa <cgiovanni1996@gmail.com>
2022-06-05 21:29:30 +0200

    :tada: initial commit

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "@pages/Home/Home";
+import { Map } from "@pages/Map/Map";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mappa" element={<Map />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
