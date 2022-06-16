69ebfdf301be5d6f3db1aa07eee2791411fca94e
Giovanni Costa <cgiovanni1996@gmail.com>
2022-06-16 18:44:05 +0200

    :sparkles: feat(home): hero section beta

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,37 @@
-import { Button } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import { APP } from "@vie/constants";
 import { useTranslation } from "react-i18next";
 
-import { Navigation } from "@vie/modules/Map/Navigation";
 export const HomePage = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <Button variant="contained">{t("name")}</Button>
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={12}>
+        <img
+          src="/media/logo.png"
+          alt="Hero img"
+          width="80%"
+          style={{ objectFit: "cover", margin: "0 auto" }}
+        />
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={12}
+        sx={{ paddingTop: "0 !important", marginTop: -3 }}
+      >
+        <Container sx={{ width: "100%" }}>
+          <Typography
+            textAlign={"center"}
+            fontWeight="bold"
+            sx={{ fontSize: "55px", wordSpacing: "-4px" }}
+          >
+            VIE DI ARDESIA
+          </Typography>
+        </Container>
+      </Grid>
+    </Grid>
   );
 };
