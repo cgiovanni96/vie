8f0235b0e7502c454739c1c8e0eb0637ca34858d
Giovanni Costa <cgiovanni1996@gmail.com>
2022-06-18 19:54:13 +0200

    :sparkles: feat(home): hero section beta.2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,19 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
 import { APP } from "@vie/constants";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
+import { green } from "@mui/material/colors";
 
 export const HomePage = () => {
   const { t } = useTranslation();
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} md={12}>
-        <img
-          src="/media/logo.png"
+      <Grid item xs={12} md={12} sx={{ marginTop: 3 }}>
+        <motion.img
+          src="/media/hero-logo.png"
           alt="Hero img"
-          width="80%"
+          width="60%"
           style={{ objectFit: "cover", margin: "0 auto" }}
         />
       </Grid>
@@ -20,16 +22,32 @@ export const HomePage = () => {
         item
         xs={12}
         md={12}
-        sx={{ paddingTop: "0 !important", marginTop: -3 }}
+        sx={{ paddingTop: "0 !important", marginTop: -1 }}
       >
         <Container sx={{ width: "100%" }}>
           <Typography
             textAlign={"center"}
             fontWeight="bold"
-            sx={{ fontSize: "55px", wordSpacing: "-4px" }}
+            sx={{ fontSize: "44px", wordSpacing: "-4px" }}
           >
-            VIE DI ARDESIA
+            <span style={{ color: green[400] }}>Vie</span> di Ardesia
           </Typography>
+
+          <Container
+            sx={{ width: "90%", margin: "0 auto", marginTop: "-10px" }}
+          >
+            <Typography
+              textAlign={"center"}
+              sx={{ fontSize: "28px" }}
+              lineHeight={"1.4rem"}
+            >
+              Un nuovo strumento per i{" "}
+              <span style={{ color: green[400], fontWeight: "bold" }}>
+                nostri
+              </span>{" "}
+              sentieri
+            </Typography>
+          </Container>
         </Container>
       </Grid>
     </Grid>
