825c4cd0ba76b263e88bd46bd3d9eb589f36dad1
Giovanni Costa <cgiovanni1996@gmail.com>
2022-06-20 23:42:13 +0200

    :sparkles: feat(home): hero section mobile

--- src/modules/Home/Hero/index.tsx
+++ src/modules/Home/Hero/index.tsx
@@ -1,6 +1,12 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import ArrowIcon from "@mui/icons-material/ArrowForward";
 
 export const Hero = () => {
+  const { t } = useTranslation();
+
   return (
     <Stack sx={{ flexDirection: { xs: "column", lg: "row" } }}>
       <Container
@@ -22,9 +28,53 @@ export const Hero = () => {
         />
       </Container>
 
-      <Container sx={{ flex: { lg: 1 } }}>
-        <Typography>Hello</Typography>
+      <Container
+        sx={(theme) => ({
+          flex: { lg: 1 },
+          display: "flex",
+          flexDirection: "column",
+          alignItems: { xs: "center", lg: "flex-start" },
+          marginTop: { xs: theme.spacing(2), lg: "none" },
+        })}
+      >
+        <Typography sx={{ fontSize: "48px", fontWeight: "bold" }}>
+          <Box component="span" sx={{ color: green[700] }}>
+            {" "}
+            Vie{" "}
+          </Box>
+          di Ardesia
+        </Typography>
+
+        <Typography
+          textAlign="center"
+          lineHeight="2rem"
+          sx={{
+            fontSize: "36px",
+            width: { xs: "90%", lg: "100%" },
+            "& span": { color: green[700], fontWeight: "bold" },
+          }}
+        >
+          <p dangerouslySetInnerHTML={{ __html: t("heroSubtitle") }} />
+        </Typography>
       </Container>
+
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          justifyContent: "center",
+          marginTop: theme.spacing(2),
+        })}
+      >
+        <Button
+          component={Link}
+          to="/mappa"
+          variant="contained"
+          sx={{ boxShadow: "none", backgroundColor: green[700] }}
+          endIcon={<ArrowIcon />}
+        >
+          {t("heroCta")}
+        </Button>
+      </Box>
     </Stack>
   );
 };
