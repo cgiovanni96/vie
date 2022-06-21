882206a1569dea0149fd7afa9d9e693bb3f4093d
Giovanni Costa <cgiovanni1996@gmail.com>
2022-06-21 14:10:51 +0200

    :wrench: chore: env supabase

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://mzxteidxlrdbrolvnbie.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16eHRlaWR4bHJkYnJvbHZuYmllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTQzNTY4MTIsImV4cCI6MTk2OTkzMjgxMn0.8So2-qoL-u2b11CGQAj56v0d_5Ljc8-AMv-ZbtVmKt4"
+  import.meta.env.VITE_SUPABASE_TOKEN || ""
 );
